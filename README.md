aa10da5f866ecbc58b2f43d7ea1b6ce95ffb9d1d
Matias Etcheverry <matias.etcheverry9@gmail.com>
2022-12-26 23:57:16 +0100

    refactoring and comment

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # Application de l'analyse de données, des statistiques descriptives et de l'apprentissage automatique dans l'industrie aéronautique
 
+This repository is my final work on the MVA course supervised by Jérôme Lacaille.
+
 ## Objective
+
+The objective of this work is to analyse 1000 flights. The analysis is contained in the file `main.ipynb` and is divided in 4 parts:
+* loading and exploring the data
+* extract the phases and especially the climbs
+* cluster the climbs
+* link with fuel consumption
 
 ## Installation
 
+To visualize and run the main notebook, you need to
+
+```{bash}
+git clone git@github.com:MatiasEtcheve/AERO.git
+cd AERO
+pip install -r requirements.txt
+```
+
+You also need to put the file `Aircraft_01.h5` in the folder `archive/`.
+
+Then you can simply run the notebook `main.ipynb`.
+
 ## Encountered problems 
+
+I have had a lot of problems when working on this project. 
+The main problem was dealing with Plotly widgets. 
+While Plotly offers a nice interactive views, the widgets don't work well / or at all when using 
+Windows Subsytem for Linux WSL2.
+This bug is not fixed, and I will open an issue on ipywidget. 
+Currently, it looks like the version `ipywidgets==7.7.2` works the best.
+
+## Usefulness of the files
+
+This repository is quite simple:
+
+| File/folder  | Usefulness                                                                                                                                                           |
+|--------------|----------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `archive/`   | Folder containing the dataset. The main notebook also write to this folder to save the climbs.                                                                       |
+| `tabata`     | Tabata toolbox from `https://github.com/jee51/tabata`. Modified to add 2 features:<br>* creating a dataset from a generator<br>* seeing a phase with multiple values |
+| `checks.py`  | Contains all the basic checks to perform on a dataset                                                                                                                |
+| `main.ipynb` | Main work                                                                                                                                                            |
+| `utils.py`   | Contains functions to extract sequence in DataFrames                                                                                                                 |
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
+from typing import Dict
 from typing import Optional
 
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+from tabata import Opset
 
 
 def extract_consecutive_sequences(
@@ -96,14 +101,11 @@ def clean_on_length(serie: pd.Series, minimum_duration: int = 2 * 60) -> pd.Seri
     """Clean a serie by blacklisting high frequencies.
     To do so, it deletes the equal consecutive sequences of length inferior to `minimum_duration`.
     This function is much faster than `clean_on_mode`.
-
     Args:
         serie (pd.Series): serie to clean
         minimum_duration (int, optional): maximum size of the sequence to delete. Defaults to 2*60 (= 60min).
-
     Returns:
         pd.Series: cleaned serie
-
     Example:
         For instance, let a serie:
         ```
@@ -116,7 +118,6 @@ def clean_on_length(serie: pd.Series, minimum_duration: int = 2 * 60) -> pd.Seri
         | 5 | 1 |
         | 6 | 0 |
         ```
-
         Then, `clean_on_length(serie, minimum_duration=2)` will return
         ```
         | index |  |
@@ -138,3 +139,66 @@ def clean_on_length(serie: pd.Series, minimum_duration: int = 2 * 60) -> pd.Seri
     sequences = extract_consecutive_sequences(serie, minimum_duration=minimum_duration)
     sequences.apply(update_df_cl, axis=1)
     return serie
+
+
+def compute_attempted_sequence(
+    df: pd.DataFrame,
+    true_value: int = 1,
+    minimum_distance: int = 2,
+    minimum_duration: int = 20,
+    output_column: str = "CL",
+) -> pd.DataFrame:
+    """
+    Computes the sequences of continuous values in `df[output_column] == true_value`.
+
+    Args:
+        df (pd.DataFrame): DataFrame to inspect
+        true_value (int): value of the cluster in `df[output_column]` to extract sequences from. Defaults to 1.
+        minimum_distance (int): minimum distance between 2 continuous sequences in `df[output_column] == true_value` to be considered different sequence. Defaults to 2.
+        minimum_duration (int): minimum duration of a sequence in `df[output_column] == true_value` to be considered. Defaults to 20.
+        output_column (str): column containing the clustering predictions. Defaults to "CL"
+
+    Returns:
+        pd.DataFrame: Dataframe where each row represents a sequence of continuous values in the DataFrame `df[output_column]`
+    """
+    whitelisted_sequences = extract_consecutive_sequences(
+        df[output_column], minimum_duration=minimum_duration
+    )
+    sequences = whitelisted_sequences[whitelisted_sequences["output_col"] == true_value]
+    sequences["distance"] = (
+        sequences["beginning"].shift(-1) - sequences["ending"]
+    ).fillna(np.inf)
+
+    return sequences[sequences["distance"] >= minimum_distance]
+
+
+def check_aborted_landings(
+    ds: Opset,
+    minimum_distance: int = 2,
+    minimum_duration: int = 20,
+    output_column: str = "CL",
+) -> Dict[str, np.ndarray]:
+    """
+    Compute the aborted landings in a dataset
+
+    Args:
+        ds (Opset): dataset of the flights
+        minimum_distance (int): minimum distance between 2 continuous sequences in `df[output_column] == true_value` to be considered different sequence. Defaults to 2.
+        minimum_duration (int): minimum duration of a sequence in `df[output_column] == true_value` to be considered. Defaults to 20.
+        output_column (str): column containing the clustering predictions. Defaults to "CL"
+
+
+    Returns:
+        Dict[str, np.ndarray]: dict mapping:
+            record_name -> [list of starting index of aborted landings]
+    """
+    ds.rewind()
+    true_value = ds.df["CL"].iloc[1200]
+    sequences = {}
+    for df in tqdm(ds):
+        current_sequences = compute_attempted_sequence(
+            df, true_value, minimum_distance, minimum_duration, output_column
+        )
+        if len(current_sequences) > 1:
+            sequences[df.index.name] = current_sequences["beginning"].values
+    return sequences
